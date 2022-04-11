81b53c62a69b99fd36cecd128e219a6413af725d
Aaditya Taparia <aadityataparia@gmail.com>
2022-04-11 21:51:16 +0900

    fix: remove mocha

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,7 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es6: true,
-    mocha: true
-  },
-  // for tests
-  globals: {
-    ENV: true,
-    chai: false,
-    sinon: false,
-    assert: false,
-    expect: false,
-    should: false,
-    delay: false,
-    port: false,
-    PATH: false,
-    SPATH: false,
-    page: false,
-    browser: false,
-    pdescribe: false
+    es6: true
   },
   parserOptions: {
     sourceType: 'module',
@@ -28,9 +11,7 @@ module.exports = {
     esversion: 2018
   },
   extends: ['plugin:prettier/recommended', 'eslint:recommended'],
-  plugins: ['mocha'],
   rules: {
-    'mocha/no-exclusive-tests': 'error',
     camelcase: [2, { properties: 'always' }],
     'consistent-return': 'error',
     'no-delete-var': 'error',
